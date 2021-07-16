03053c0f5acfd4d0ab5a617ad357cdaef822a64f
Eric Mandersloot <eric@h1.nl>
2021-07-16 14:51:50 +0200

    faro shuffle

--- src/faro-shuffle/faro-shuffle.spec.ts
+++ src/faro-shuffle/faro-shuffle.spec.ts
@@ -1,21 +1,22 @@
 /*jshint esversion: 6 */
-import {applyN, separate, riffle, shuffle} from './index';
+import {applyN, separate, riffle, shuffle, shuffleTimesN} from './index';
 
-test('apply a function N times', () => {
+test('Apply a function N times', () => {
   expect(applyN((x: number) => x + 1, 5)(1)).toBe(6);
   expect(applyN((x: number) => x * x, 4)(2)).toBe(65536);
 });
 
-test('seperate an array in N (sort of) equal parts', () => {
-  expect(separate(2, [1, 2, 3, 4, 5, 6])).toEqual([
+test('Seperate an array in N (sort of) equal parts', () => {
+  expect(separate([1, 2, 3, 4, 5, 6])).toEqual([
     [1, 2, 3],
     [4, 5, 6],
   ]);
-  expect(separate(3, [1, 2, 3, 4, 5, 6])).toEqual([
+  expect(separate([1, 2, 3, 4, 5, 6], 3)).toEqual([
     [1, 2],
     [3, 4],
     [5, 6],
   ]);
+  expect(separate([])).toEqual([]);
 });
 
 test('Merge 2 arrays maintaining index priority', () => {
@@ -27,13 +28,18 @@ test('Merge 2 arrays maintaining index priority', () => {
   ).toEqual([1, 6, 2, 7, 3, 8, 4, 9, 5, 10]);
 });
 
+test('Shuffle an integer array', () => {
+  expect(shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toEqual([
+    1, 6, 2, 7, 3, 8, 4, 9, 5, 10,
+  ]);
+});
+
 test('Shuffle a deck of cards 8 times', () => {
   const cards: number[] = Array(52)
     .fill(0)
     .map((value, index) => index);
-  expect(applyN(shuffle, 8)(cards)).toEqual(
-    Array(52)
-      .fill(0)
-      .map((value, index) => index)
-  );
+  const newDeckOrder = Array(52)
+    .fill(0)
+    .map((value, index) => index);
+  expect(shuffleTimesN(cards, 8)).toEqual(newDeckOrder);
 });
--- src/faro-shuffle/index.ts
+++ src/faro-shuffle/index.ts
@@ -1,25 +1,45 @@
 /*jshint esversion: 6 */
-
 /*
-  Do a faro shuffle times 8
-  Faro shuffle.
+
+  Faro Shuffle a deck of cards.
+  Faro shuffling is a method of shuffling playing cards, in which two halfs of the deck interleave perfectly.
+
+  A perfect Faro shuffle should do two thing:
+
+  1. seperate(cards) : Split an array of integers in to 2 equal parts.
+  2. riffle(cards): Merge the chunked arrays from right to left, while maintaining the original index.
+     riffle([[1,2,3],[4,5,6]]) should return [1,4,2,5,3,6]
+
+  Create a function shuffle(cards) that accepts an integer array as a parameter
+  and both seperates and riffle shuffles the cards.
+
+  Questions:
+
+  A. How many Faro shuffles does it take to return to the original order of the deck?
+  Spoiler alert: every amateur magician knows the answer, show your calculations.
+
 */
 
 const R = require('ramda');
 
-export const applyN = R.compose(R.reduceRight(R.compose, R.identity), R.repeat);
-
-export const separate = (n: number, list: number[]): number[][] => {
-  const length = list.length;
-  const fn = (_v: number, idx: number) => Math.floor((idx * n) / length);
+export const separate = (
+  list: number[],
+  n: number = 2
+): number[][] | number[] => {
+  const fn = (_v: number, i: number): number =>
+    Math.floor((i * n) / list.length);
   return R.values(R.addIndex(R.groupBy)(fn, list));
 };
 
-export const riffle = (parts: number[][]): number[] =>
-  parts[0].map((c: number, i: number) => [parts[0][i], parts[1][i]]).flat(1);
+export const toPairs = (parts: number[][]): number[][] =>
+  parts[0].map((c: number, i: number) => [parts[0][i], parts[1][i]]);
 
-export const shuffle = (cards: number[]): number[] | number[][] => {
-  const parts = separate(2, cards);
-  cards = riffle(parts);
-  return cards;
-};
+export const riffle = (parts: number[][]): number[] => toPairs(parts).flat(1);
+
+export const applyN = R.compose(R.reduceRight(R.compose, R.identity), R.repeat);
+
+export const shuffle = (cards: number[]): number[] =>
+  R.pipe(separate, riffle)(cards);
+
+export const shuffleTimesN = (cards: number[], n: number): number[] =>
+  applyN(shuffle, n)(cards);
